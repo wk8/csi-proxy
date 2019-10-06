f9e64997da28ceb68abc3acb6966841fbd08c3ef
Jean Rouge <rougej+github@gmail.com>
2019-10-06 10:31:34 -0700

    WIP

--- integrationtests/csi-api-gen/new-group/api/doc.go
+++ integrationtests/csi-api-gen/new-group/api/doc.go
@@ -2,5 +2,6 @@ package api
 
 // The comment below indicates that this package defines a CSI-proxy API group
 // See comments in this repo's cmd/csi-api-gen/generators/api.go file for more context.
+// +csi-proxy-gen=groupName:new_group
 // +csi-proxy-gen=serverBasePkg:csi-api-gen/new_group/actual_output/server
 // +csi-proxy-gen=clientBasePkg:csi-api-gen/new_group/actual_output/client
